e0cbbe6687ed9d5e97a34ff36a8830138a1b6317
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-03-22 22:02:13 -0300

    creacion de las rutas relacionadas con el modelo films

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const apiRouter = require("./routes/api");
 
 const { PORT } = process.env;
 
@@ -11,9 +12,7 @@ require("./db");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Hola Mundo");
-});
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
     console.log("Servidor funcionando.");
