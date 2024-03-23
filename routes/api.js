866ae3f3a5442d0ca10f95614661c46685f17515
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-03-22 23:35:39 -0300

    creacion del middleware de genera el token al acceder el usuario a la app

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
+const middlewares = require("./middlewares")
+
 const apiFilmsRouter = require("./api/films");
 const apiUserRouter = require("./api/users")
 
-router.use("/films", apiFilmsRouter);
+router.use("/films", middlewares.checkToken, apiFilmsRouter);
 router.use("/users", apiUserRouter);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,11 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const moment = require("moment");
+const jwt = require("jwt-simple");
 
 const { check, validationResult } = require("express-validator");
 
 const { User} = require("../../db");
-
-router.get("/", async (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
-});
 
 router.post("/register", [
     check( "username", "El nombre de usuario es obligatorio.").not().isEmpty(),
@@ -23,18 +21,25 @@ router.post("/register", [
     res.json(user);
 });
 
-router.put("/:filmId", async (req, res) => {
-    await Film.update(req.body, {
-        where: { id: req.params.filmId },
-    });
-    res.json({ success: "Se ha modificado el registro." });
-});
+router.post("/login", async (req, res)=> {
+    const user = await User.findOne({where : { email: req.body.email}});
+    if(user){
+        const iguales = bcrypt.compareSync(req.body.password, user.password);
+        if(iguales){
+            res.json({success: createToken(user)})
+        }
+    } else {
+        res.json({error: "Error en usuario y/o contraseña."})
+    }
+})
 
-router.delete("/:filmId", async (req, res) => {
-    await Film.destroy({
-        where: { id: req.params.filmId },
-    });
-    res.json({ success: "Se ha borrado el registro." });
-});
+const createToken = (user) => {
+    const payload = {
+        userId: user.id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(5, "minutes").unix()
+    }
 
+    return jwt.encode(payload, "frase-secreta");
+}
 module.exports = router;
